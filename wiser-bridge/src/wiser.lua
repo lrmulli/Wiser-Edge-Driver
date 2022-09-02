dc57f8ba13d1b7863ff477c9226bad2f9acb11d8
lrmulli <lmullineux@gmail.com>
2022-09-02 15:57:09 +0100

    added 1st api call

--- wiser-bridge/src/wiser.lua
+++ wiser-bridge/src/wiser.lua
@@ -7,6 +7,22 @@ local wiser = {}
 function wiser.makeApiGetCall(driver,device,path)
   local url = "http://"..device.preferences.deviceaddr..path
   log.debug("Making API Call to ",url)
+  local respbody = {} -- for the response body
+  http.TIMEOUT = 50;
+  log.info("Sending request...")
+
+  local result, respcode, respheaders, respstatus = http.request {
+    method = "GET",
+    url = url,
+    headers = {
+        ["Secret"] = secret
+    },
+    sink = ltn12.sink.table(respbody)
+    }
+  -- get body as string by concatenating table filled by sink
+  respbody = table.concat(respbody)
+  log.debug("Response Body "..respbody)
+  print(result,respcode,respstatus)
 end
 
 return wiser
