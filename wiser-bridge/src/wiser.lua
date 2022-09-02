1226b332cbbb909fe3e3b9ff551f9f2bf592b368
lrmulli <lmullineux@gmail.com>
2022-09-02 16:07:46 +0100

    added missing libs

--- wiser-bridge/src/wiser.lua
+++ wiser-bridge/src/wiser.lua
@@ -1,5 +1,10 @@
 local log = require "log"
 local capabilities = require "st.capabilities"
+local socket = require'socket'
+local json = require "dkjson"
+local cosock = require "cosock"
+local http = cosock.asyncify "socket.http"
+ltn12 = require("ltn12")
 
 local wiser = {}
 
