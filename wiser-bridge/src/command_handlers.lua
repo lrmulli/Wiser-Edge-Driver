b502d6c38504d5686393ddc786bfe163d3909d8b
lrmulli <lmullineux@gmail.com>
2022-09-23 12:19:52 +0100

    initial commits

--- wiser-bridge/src/command_handlers.lua
+++ wiser-bridge/src/command_handlers.lua
@@ -9,6 +9,7 @@ function command_handlers.switch_on(driver, device, command)
   log.debug(string.format("[%s] calling set_power(on)", device.device_network_id))
   device:emit_event(capabilities.switch.switch.on())
   wiser.createRooms(driver,device,payload)
+  wiser.refreshRooms(driver,device)
 end
 
 -- callback to handle an `off` capability command
