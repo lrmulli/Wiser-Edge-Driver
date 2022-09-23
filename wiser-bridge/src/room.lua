cf0137251de023b41ff910035616f3b253a8f2b8
lrmulli <lmullineux@gmail.com>
2022-09-23 08:51:23 +0100

    initial commits

--- wiser-bridge/src/room.lua
+++ wiser-bridge/src/room.lua
@@ -2,15 +2,15 @@ local capabilities = require "st.capabilities"
 local Driver = require "st.driver"
 local log = require "log"
 
-local hbactivity_command_handlers = require "hbactivity_command_handlers"
+local room_command_handlers = require "room_command_handlers"
 
 
 local room_handler = {
 NAME = "RoomHandler",
   capability_handlers = {
     [capabilities.switch.ID] = {
-        [capabilities.switch.commands.on.NAME] = hbactivity_command_handlers.switch_on,
-        [capabilities.switch.commands.off.NAME] = hbactivity_command_handlers.switch_off,
+        [capabilities.switch.commands.on.NAME] = room_command_handlers.switch_on,
+        [capabilities.switch.commands.off.NAME] = room_command_handlers.switch_off,
         }
     },
   can_handle = function(opts, driver, device, ...)
