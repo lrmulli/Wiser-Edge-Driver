c644ee21519d4ed0d2e4c0352375e8c49cb63974
lrmulli <lmullineux@gmail.com>
2022-09-23 13:12:13 +0100

    initial commits

--- wiser-bridge/src/room_command_handlers.lua
+++ wiser-bridge/src/room_command_handlers.lua
@@ -23,12 +23,8 @@ function room_command_handlers.switch_on(driver, device, command)
     end
     log.info(utils.stringify_table(update,"Room Update Message: ",true))
     --set current temperature
-    local temp = update.CalculatedTemperature / 10
-    local temp = {
-        value = update.CalculatedTemperature / 10,
-        unit = "C"
-    }
-    device:emit_component_event(device.profile.components.roomtemperature,capabilities.temperatureMeasurement.temperature(temp))
+    local temp = {value = update.CalculatedTemperature / 10, unit = "C"}
+    device:emit_event(capabilities.temperatureMeasurement.temperature(temp))
 
   end
 
